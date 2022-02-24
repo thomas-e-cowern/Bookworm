9c786ec9edf3c482c184aa941f1b6aa6ab1d41ba
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-02-24 06:33:15 -0500

    initial commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Thomas Cowern New on 2/24/22.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
