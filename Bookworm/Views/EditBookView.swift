15171d342cd210ce614314d7e856042d8279adeb
thomas-e-cowern <thomas.e.cowern@gmail.com>
2022-02-25 06:12:19 -0500

    added edit book view

---
+++ Bookworm/Views/EditBookView.swift
@@ -0,0 +1,20 @@
+//
+//  EditBookView.swift
+//  Bookworm
+//
+//  Created by Thomas Cowern New on 2/25/22.
+//
+
+import SwiftUI
+
+struct EditBookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditBookView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditBookView()
+    }
+}
